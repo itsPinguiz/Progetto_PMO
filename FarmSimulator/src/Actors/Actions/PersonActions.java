912da2b55ef52b5c45f880c13269982a7332546d
itsPinguiz <93825368+itsPinguiz@users.noreply.github.com>
2022-12-02 17:00:17 +0100

    Changed UML

--- FarmSimulator/src/Actors/Actions/PersonActions.java
+++ FarmSimulator/src/Actors/Actions/PersonActions.java
@@ -3,7 +3,12 @@ package Actors.Actions;
 import Actors.Person.Person;
 
 public class PersonActions extends Actions{
+<<<<<<< HEAD
     private final Person person;
+=======
+    private final Object person;
+    //private final Person place;
+>>>>>>> 29c8d7d (Changed UML)
 
     protected PersonActions(Person person){
         super();
@@ -14,6 +19,7 @@ public class PersonActions extends Actions{
         /*
          * Method to find a method to execute
          */
+<<<<<<< HEAD
         // find the method and execute it
         try {
            this.getClass().getMethod(s).invoke(null);
@@ -22,6 +28,10 @@ public class PersonActions extends Actions{
             System.out.println("Wasn't able to execute action due to " + e);
         } 
     }
+=======
+        Method method = null;
+        //this.place = person.getplace
+>>>>>>> 29c8d7d (Changed UML)
 
     public void grabItem(){
         /*
--- README.md
+++ README.md
@@ -12,5 +12,13 @@ Repository del progetto di PMO della sessione invernale 2022/23
 # Specifica del progetto
 Il progetto consiste nell'implementazione di un simulatore di fattoria.
 
+Funzioni principali:
+* Si potrà assumere il ruolo di proprietario e agricoltore
+* Il proprietario potrà acquistare e vendere le porzioni di terreno, così come le risorse necessarie
+* L’agricoltore dovrà lavorare la porzione di terreno per permettere la crescita di piante o l’allevamento di animali 
+* Ogni porzione di terreno può ospitare animali  o piante
+* Un deposito per la conservazione di strumenti, risorse e animali
+* Un negozio da cui acquistare i semi, risorse, animali o terreno
+* Variazione delle stagioni che influenza il tempo atmosferico
 
 
