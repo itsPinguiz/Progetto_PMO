25d523216d17427ca917dffac8703182c895373e
Luis <l.frasheri9@campus.uniurb.it>
2022-12-02 23:48:21 +0100

    Stefano's upgrade

--- FarmSimulator/src/Actors/Actions/PersonActions.java
+++ FarmSimulator/src/Actors/Actions/PersonActions.java
@@ -1,14 +1,26 @@
 package Actors.Actions;
 
+<<<<<<< HEAD
+=======
+import java.lang.reflect.Method;
+>>>>>>> 388d23a (Stefano's upgrade)
 import Actors.Person.Person;
 
 public class PersonActions extends Actions{
+<<<<<<< HEAD
 <<<<<<< HEAD
     private final Person person;
 =======
     private final Object person;
     //private final Person place;
 >>>>>>> 29c8d7d (Changed UML)
+=======
+    private final Object person;
+    //private final Person place;
+=======
+    private final Person person;
+>>>>>>> fee66ca (Replaced Objects and Hashmap to Hashset)
+>>>>>>> 388d23a (Stefano's upgrade)
 
     protected PersonActions(Person person){
         super();
@@ -31,7 +43,25 @@ public class PersonActions extends Actions{
 =======
         Method method = null;
         //this.place = person.getplace
+<<<<<<< HEAD
 >>>>>>> 29c8d7d (Changed UML)
+=======
+
+        try {
+            method = this.getClass().getMethod(s);
+            } catch (SecurityException e) { 
+            System.out.println("Wasn't able to execute action.");
+            }
+            catch (NoSuchMethodException e) { 
+                System.out.println("Action '" + s + "' is not present in the current class." + this.getClass().toString());
+            }
+                catch (NullPointerException  e) { 
+                System.out.println("Action is null.");
+            }
+
+        return method;
+    }
+>>>>>>> 388d23a (Stefano's upgrade)
 
     public void grabItem(){
         /*
