d0d8d48d70f0551665c22e62de967afedca89037
idef011 <f.belhaj@campus.uniurb.it>
2022-12-05 22:45:19 +0100

    added interface asbtract and classes of land

---
+++ FarmSimulator/src/Land/LandAbstract.java
@@ -0,0 +1,10 @@
+package Land;
+
+import java.util.ArrayList;
+
+public abstract class LandAbstract implements LandInteface {
+    protected float price;
+    protected String type;
+    protected String color;
+    protected int levelWater;
+}
---
+++ FarmSimulator/src/Land/LandAnimal.java
@@ -0,0 +1,39 @@
+package Land;
+
+import java.util.ArrayList;
+
+import Actors.Actions.PlaceActions;
+import Animal.AnimalAbstract;
+
+public class LandAnimal extends LandAbstract {
+    //attributes of land animal
+    ArrayList<AnimalAbstract> elements;
+    PlaceActions actionAnimal;
+    
+    //constructor to do
+
+    @Override
+    public String getLandType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getNumElements() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getLandColor() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public float getPrice() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
