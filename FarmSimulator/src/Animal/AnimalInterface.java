eb83f45b79c2ae966818c9da95c25dca9def8982
idef011 <f.belhaj@campus.uniurb.it>
2022-12-05 21:09:35 +0100

    added inerface abstract and class of animal

--- FarmSimulator/src/Animal/AnimalInterface.java
+++ FarmSimulator/src/Animal/AnimalInterface.java
@@ -1,5 +1,9 @@
 package Animal;
 interface AnimaliInterface {
+public int getHunger();
+public int getLife();
+public String getAnimalType();
+public int getProducts();
 
     
 }
---
+++ FarmSimulator/src/Animal/Cow.java
@@ -0,0 +1,29 @@
+package Animal;
+
+public class Cow extends AnimalAbstract {
+
+    @Override
+    public int getHunger() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getLife() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getAnimalType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getProducts() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
