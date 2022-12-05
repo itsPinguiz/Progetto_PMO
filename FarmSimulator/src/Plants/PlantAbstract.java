af12a1f5374f8d6e636da5d39259103e11510366
idef011 <f.belhaj@campus.uniurb.it>
2022-12-05 21:25:54 +0100

    added interface and abstract of plants

---
+++ FarmSimulator/src/Plants/PlantAbstract.java
@@ -0,0 +1,5 @@
+package Plants;
+
+public abstract class PlantAbstract  {
+    
+}
---
+++ FarmSimulator/src/Plants/PlantInteface.java
@@ -0,0 +1,9 @@
+package Plants;
+
+public interface PlantInteface {
+    public String getPlantType();
+    public int getLife();
+    public int getProduct();
+    public boolean isPlantable();
+
+}
