f1157ed75eac4865106520fd669dc43cc947d75e
idef011 <f.belhaj@campus.uniurb.it>
2022-12-13 16:12:21 +0100

    added calendar to AnimalAbstract,implemented some methods of chicken

--- FarmSimulator/src/Animal/Chicken.java
+++ FarmSimulator/src/Animal/Chicken.java
@@ -1,29 +1,52 @@
 package Animal;
 
+import Calendar.Calendar;
+
 public class Chicken extends AnimalAbstract{
+
+    //contructor
+    Chicken(Calendar c){
+       this.life = 100;
+       this.hunger = 0;
+       this.nProduct = 0;
+       this.name = "chicken";
+       this.typeProduct = "meat";
+       this.c = c;
+    }
+    //method for changing hunger of chicken
+    public void setHunger(){
+        //check if life == 0
+        this.hunger = (1/this.life)*c.getDay();
+    }
+
+    //method for changing life of chicken
+    public void setLife(){
+        this.life = (1/this.life)*c.getDay();
+    }
 
     @Override
     public int getHunger() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.hunger;
     }
 
     @Override
     public int getLife() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.life;
     }
 
     @Override
-    public String getAnimalType() {
-        // TODO Auto-generated method stub
-        return null;
+    public String getAnimalType() {       
+        return this.typeProduct;
     }
 
     @Override
+    //every time you call this method the number of 
+    //product are set to zero
     public int getProducts() {
-        // TODO Auto-generated method stub
-        return 0;
+
+        int tmp = this.nProduct;
+        this.nProduct = 0;        
+        return tmp;
     }
     
 }
