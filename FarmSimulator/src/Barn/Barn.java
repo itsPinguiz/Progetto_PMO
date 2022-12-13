195a684658cec9acd846662d3df94b6be66173bf
idef011 <f.belhaj@campus.uniurb.it>
2022-12-13 18:20:39 +0100

    added class barn

---
+++ FarmSimulator/src/Barn/Barn.java
@@ -0,0 +1,13 @@
+package Barn;
+
+import java.util.ArrayList;
+
+import Actors.Actions.PlaceActions;
+import Animal.AnimalAbstract;
+import Item.Interface.Item;
+
+public class Barn {
+    private PlaceActions actionBarn;
+    private ArrayList<AnimalAbstract> stall;
+    private ArrayList<Item> items;
+}
