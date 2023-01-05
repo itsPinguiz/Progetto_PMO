1507d3b9c1afb4623f9b5142b10ba3bf2fdd82dc
idef011 <f.belhaj@campus.uniurb.it>
2023-01-05 09:55:12 +0100

    added classes Chunk and ChunkPlantLand and removed LandPlant

---
+++ FarmSimulator/src/Land/Chunk.java
@@ -0,0 +1,13 @@
+package Land;
+
+import Plants.PlantAbstract;
+
+public class Chunk {
+   
+    private PlantAbstract aplant;
+
+    Chunk(PlantAbstract plant){
+        this.aplant = plant;
+    }
+
+}
