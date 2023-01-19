1fdfcf844c52dc308b3c36281faa683e3a3bac5f
itsPinguiz <93825368+itsPinguiz@users.noreply.github.com>
2023-01-19 21:24:46 +0100

    polishing

--- FarmSimulator/src/Land/LandAbstract.java
+++ FarmSimulator/src/Land/LandAbstract.java
@@ -2,6 +2,7 @@ package Land;
 
 public abstract class LandAbstract implements LandInteface {
     static final float BASE_PRICE_LAND = 50; //initial price of a land 
+    protected boolean sellable; // decides whether a land can be sold or not
     protected float price;
     protected String type;
     
