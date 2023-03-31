d82585cfec496dd9ed5d2431a92e0870dc1af61c
itsPinguiz <93825368+itsPinguiz@users.noreply.github.com>
2023-03-31 11:09:20 +0200

    merged all plants in one general class

---
+++ FarmSimulator/src/Item/Plants/species/Plant.java
@@ -0,0 +1,13 @@
+package Item.Plants.species;
+
+import Item.ItemType.Plants;
+import Item.Plants.PlantAbstract;
+
+public class Plant extends PlantAbstract {
+    
+    public Plant(Plants type){
+            super(null);
+            this.type = type;
+            this.price = 5;
+    }   
+}
