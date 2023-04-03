7ecaee3db45fcfb88bed638e59acaf15111397f8
itsPinguiz <93825368+itsPinguiz@users.noreply.github.com>
2023-04-03 11:10:50 +0200

    fixed minor bug with serialization

--- FarmSimulator/src/Inventory/InventoryInterface.java
+++ FarmSimulator/src/Inventory/InventoryInterface.java
@@ -1,9 +1,11 @@
 package Inventory;
 
+import java.io.Serializable;
+
 import Item.ItemType;
 import Item.Interface.Item;
 
-public interface InventoryInterface {
+public interface InventoryInterface extends Serializable {
     public void addItem(Item newItem) throws Exception;
     public void removeItem(Item item) throws Exception;
     public Item getItem(int numItemReq, Item itemRequest) throws CloneNotSupportedException;
